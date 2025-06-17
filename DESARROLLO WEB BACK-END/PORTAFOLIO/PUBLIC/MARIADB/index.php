31001250842ba4ed8e347162b0467aed8ee1623f
Giuseppe <giusepestu@gmail.com>
2025-06-17 16:55:58 +0100

    022_backend: Se modifica el portafolio.

--- DESARROLLO WEB BACK-END/PORTAFOLIO/PUBLIC/MARIADB/index.php
+++ DESARROLLO WEB BACK-END/PORTAFOLIO/PUBLIC/MARIADB/index.php
@@ -68,21 +68,23 @@ if ( $_SERVER['REQUEST_METHOD'] == 'POST') {
 <body>
 <header class="Encabezado_Google" id="miEncabezado">
 	<img class="Logo_Google" src="../../ASSETS/IMAGES/Google_2015_logo.svg.png" alt="Google_Logo">
-	<div class="Buscador">
-		<form action="" method="post">
+	<div id="mensaje_1" class="Buscador">
+		<form id="mensaje_5" method="post">
+			<div id="buscador"></div>
 			<input id="mensaje" name="busqueda" type="text" class="Buscador_">
-			<button class="Boton_1" type="reset">
-				<svg class="Imagen_Boton_1" focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path></svg>
+			<button id="buscador_2" class="Boton_1" type="reset">
+			<svg class="Imagen_Boton_1" focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path></svg>
 	        </button>
 		<div class="Barra_lateral"></div>
 		<button disabled class="Boton_2"><svg class="Imagen_Boton_2" focusable="false" viewBox="0 -960 960 960" xmlns="http://www.w3.org/2000/svg"><path fill="var(--bbQxAb)" d="M480-400q-50 0-85-35t-35-85v-240q0-50 35-85t85-35q50 0 85 35t35 85v240q0 50-35 85t-85 35Zm-40 280v-123q-104-14-172-93t-68-184h80q0 83 58.5 141.5T480-320q83 0 141.5-58.5T680-520h80q0 105-68 184t-172 93v123h-80Z"></path></svg></button>
-		<button id="enviar" class="Boton_3"><svg  class="Imagen_Boton_3" focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path></svg></button>
+		<button  id="enviar" class="Boton_3"><svg  class="Imagen_Boton_3" focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path></svg></button>
 		</form>
+		<div id="mensaje_4">
+		</div>
 	</div>
 	<div class="Elementos_Derecha">
-
 		<div class="Imagen_fondo">
-			<svg class="Imagen_Boton_4" focusable="false" viewBox="0 0 24 24"><path d="M6,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM16,6c0,1.1 0.9,2 2,2s2,-0.9 2,-2 -0.9,-2 -2,-2 -2,0.9 -2,2zM12,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2z"></path></svg>
+		<a href="http://localhost/phpmyadmin/"><svg class="Imagen_Boton_4" focusable="false" viewBox="0 0 24 24"><path d="M6,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM16,6c0,1.1 0.9,2 2,2s2,-0.9 2,-2 -0.9,-2 -2,-2 -2,0.9 -2,2zM12,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2z"></path></svg></a>	
 		</div>
 	    <img  class="Logo_Usuario" src="../../ASSETS/IMAGES/Diseño sin título-Photoroom.png" alt="Logo_Usuario">
     </div>
@@ -96,7 +98,7 @@ if ( $_SERVER['REQUEST_METHOD'] == 'POST') {
 </div>
 <div class="Linea"></div>
 
-<div class="page_content">
+<div class="page_content" id="mensaje_3">
 	<main>
 	<div class="Contenedor_Proyect">
 		<div class="Titulo_Logo">
@@ -174,6 +176,76 @@ if ( $_SERVER['REQUEST_METHOD'] == 'POST') {
 	</div>
 </footer>
 </div>
+	<script>
+  document.getElementById("mensaje_3").addEventListener("click", function () {
+    document.getElementById("mensaje_1").className = "Buscador";
+    document.getElementById("buscador").innerHTML = '';
+    document.getElementById("buscador_2").style.display = 'block';
+    const input = document.getElementById("mensaje");
+    input.className = "Buscador_";
+	document.getElementById("mensaje").value="";
+	document.getElementById("mensaje_5").style.borderBottom = "0px";
+	document.getElementById("mensaje_4").innerHTML="";
+  });
+
+window.addEventListener('pageshow', (event) => {
+  document.getElementById('mensaje').value = '';
+  document.getElementById("mensaje_1").className = "Buscador";
+  document.getElementById("buscador_2").style.display = 'block';
+  document.getElementById("buscador").innerHTML = '';
+  document.getElementById("mensaje_5").style.borderBottom = "0px";
+  document.getElementById("mensaje_4").innerHTML="";
+
+});
+
+
+
+
+  document.getElementById("mensaje_1").addEventListener("click", function () {
+	const input = document.getElementById("mensaje");
+    let texto = input.value;
+
+    input.className = "Buscador_4";
+    document.getElementById("buscador_2").style.display = 'none';
+
+	document.getElementById("mensaje_5").style.borderBottom = "1px solid #dadce0";
+
+
+	document.getElementById("mensaje_4").innerHTML='<div class="busquedas"><img src="../../ASSETS/IMAGES/reloj.png" alt=""> <p>¿Cuántos integrantes tiene el grupo Europe?</p> </div>' + '<div class="busquedas"><img src="../../ASSETS/IMAGES/reloj.png" alt=""> <p>¿Qué es el socialismo?</p> </div>'+ '<div class="busquedas"><img src="../../ASSETS/IMAGES/reloj.png" alt=""> <p>¿La Teoría del todo existe?</p> </div>'+ '<div class="busquedas"><img src="../../ASSETS/IMAGES/reloj.png" alt=""> <p>¿Cuántos planetas hay en el Universo?</p> </div>'+ '<div class="busquedas"><img src="../../ASSETS/IMAGES/reloj.png" alt=""> <p>Habitantes en China</p> </div>'+ '<div class="busquedas"><img src="../../ASSETS/IMAGES/reloj.png" alt=""> <p>Manual de Phyton</p> </div>'+ '<div class="busquedas"><img src="../../ASSETS/IMAGES/reloj.png" alt=""> <p>¿Quién empezó la Guerra de Irak?</p> </div>'+ '<div class="busquedas_"><p><i>Denunciar predicciones inadecuadas.</i></p><a href=""> Más información</a></div>';
 	
+    input.addEventListener("keydown", function () {
+	  let texto = input.value;
+      if (texto.trim() !== "") {
+        input.className = "Buscador_5";
+        document.getElementById("buscador_2").style.display = 'block';
+      } else {
+        input.className = "Buscador_4";
+        document.getElementById("buscador_2").style.display = 'none';
+      }
+    });
+
+
+    input.className = "Buscador_4";
+    document.getElementById("mensaje_1").className = "Buscador_2";
+    document.getElementById("buscador").innerHTML = `
+      <svg class="buscador_3" focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+        <path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5
+        6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5
+        4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5
+        9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
+      </svg>`;
+  });
+
+  document.addEventListener("reset", function () {
+    const input = document.getElementById("mensaje");
+    setTimeout(() => {
+      input.disabled = false;
+      input.readOnly = false;
+      input.className = "Buscador_4";
+      document.getElementById("buscador_2").style.display = "none";
+	  input.focus();
+    }, 0);
+  });
+</script>
 </body>
 </html>
